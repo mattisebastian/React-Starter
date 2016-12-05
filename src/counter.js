a7502d27300e2305cf5cf54e2fe5f7ebbe2dccd9
Matti <matti.leydecker@gmx.de>
2016-12-05 01:45:36 +0100

    added a minimal interface example

--- src/counter.js
+++ src/counter.js
@@ -1,30 +1,56 @@
 import React from 'react';
- 
-/**
- * A counter button: tap the button to increase the count.
- */
-let doctbdy;
+import { Button, Form, FormGroup, Label, Input, FormText, Container, Row, Col } from 'reactstrap';
 
-class Counter extends React.Component {
+export default class Example extends React.Component {
+
+                    
   constructor() {
     super();
-    this.state = {
-      count: 0,
-    };
+    fetch('http://localhost:9090/api/requestSla', {
+          //mode: 'no-cors' // this means the response type will be 'opaque' - whatever that means
+        }).then(function(response) { 
+      // Convert to JSON
+      return response.json();
+    }).then(function(j) {
+      // Yay, `j` is a JavaScript object
+      console.log(j); 
+      this.state = {
+        data: j
+    }
+    }.bind(this));
+    
   }
- 
-
   render() {
     return (
-      <button
-        onClick={() => {
-          this.setState({ count: this.state.count + 15});
-        }}
-      >
-        Count: {this.state.count}
+      <Container>
+        <Row>
+          <Col xs="6">
+              <Form>
+              <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" value={this.state.data.customer.name} />
+              </FormGroup>
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="exampleSelect">Select</Label>
+                <Input type="select" name="select" id="exampleSelect">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Input>
+              
+              </FormGroup>
+            </Form>
+
+          </Col>
+        </Row>
+      </Container>
       
-      </button>
     );
   }
 }
-export default Counter;
